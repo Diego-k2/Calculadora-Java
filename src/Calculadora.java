144dd2b1f6f9ba824acd7a42a309c6edf803f422
Adenilson Soares <adenilson_denis8@hotmail.com>
2021-12-28 15:25:38 -0300

    teste

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,43 +1,57 @@
 import java.util.Scanner;
+
 public class Calculadora {
 
 	public static void main(String[] args) {
-		Scanner teclado = new Scanner (System.in);
-		 int a, b;
-		 
-		 System.out.println("Digite aqui o primeiro valor");
-		 a = teclado.nextInt();
-		 System.out.println("Digite aqui o segundo valor valor");
-		 b = teclado.nextInt();
-		  
-		 int soma = soma(a,b);
-		 int subtração = subtração(a,b);
-		 int multiplicação = multiplicação (a,b);
-		 double divisão = divisão (a,b);
+		Scanner teclado = new Scanner(System.in);
+		int a, b;
+		String opc = "";
+		double resultado = 0;
 
-		 System.out.println(soma);
-		 System.out.println(subtração);
-		 System.out.println (multiplicação);
-		 System.out.println(divisão);
-		 
-		 
-	}
-	
+		System.out.println("Digite aqui o primeiro valor: ");
+		a = teclado.nextInt();
+		System.out.println("Digite o tipo de operação(+, -, * ou /): ");
+		opc = teclado.next();
+		System.out.println("Digite aqui o segundo valor valor: ");
+		b = teclado.nextInt();
+		
+		
+		//Switch para escolha de operação
+		switch (opc) {
+		case "+":
+			resultado = soma(a, b);
+			break;
+		case "-":
+			resultado = subtracao(a, b);
+			break;
+		case "*":
+			resultado = multiplicacao(a, b);
+			break;
+		case "/":
+			resultado = divisao(a, b);
+			break;
+		default:
+			System.out.println("Opção inexistente: " + opc);
+		}
 
-	
-	public static int soma (int a, int b) {
-		return  a + b;
+		System.out.println();
+		System.out.println("Resultado: " + resultado);
+
 	}
-	
-	public static int subtração (int a, int b) {
+
+	public static int soma(int a, int b) {
+		return a + b;
+	}
+
+	public static int subtracao(int a, int b) {
 		return a - b;
 	}
-	
-	public static int multiplicação (int a, int b) {
+
+	public static int multiplicacao(int a, int b) {
 		return a * b;
 	}
-	
-	public static double divisão (double a, double b) {
+
+	public static double divisao(double a, double b) {
 		return a / b;
 	}
 
